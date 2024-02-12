359ad9d2a0a06872299ba3db7cbc996f2a00c2f5
GabrielGodfirst <43904195+GabrielGodfirst@users.noreply.github.com>
2024-02-12 01:28:27 +0000

    Create BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -13,11 +13,24 @@ from datetime import datetime
 class BaseModel:
     """Defines common attributes/methods for other classes"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initialize instance attributes"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            if 'created_at' in kwargs:
+                kwargs['created_at'] = datetime.strptime(
+                        kwargs['created_at'], "%Y-%m-%dT%H:%M:%S.%f"
+                )
+            if 'updated_at' in kwargs:
+                kwargs['updated_at'] = datetime.strptime(
+                        kwargs['updated_at'], "%Y-%m-%dT%H:%M:%S.%f"
+                )
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """Return string representation of the instance"""
